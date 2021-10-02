390582bb0b089f90aa133ce00095a38af6fc8019
Esteban Olejnik <74082400+Estiby@users.noreply.github.com>
2021-10-01 22:59:46 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>PROXIMAMENTE</h1>
-        <p>
-          Ecommerce en React - Proyecto CoderHouse.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+
+        <Navbar.Brand>
+          <img src={logo} width="40px" height="40px" />{''}
+          React CoderHouse
+        </Navbar.Brand>
+
+        <NavbarToggle />
+        <NavbarCollapse>
+          <Nav>
+          <Nav.Link href="/">Principal</Nav.Link>
+            <NavDropdown title="Productos">
+              <NavDropdown.Item href="productos/chocolates">Chocoolates</NavDropdown.Item>
+              <NavDropdown.Item href="productos/tortas">Tortas</NavDropdown.Item>
+              <NavDropdown.Item href="productos/bombones">Bombones</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item href="productos/promociones">Promociones</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="nosotros">Nosotros</Nav.Link> 
+            <Nav.Link href="ofertas">Ofertas</Nav.Link> 
+            <Nav.Link href="contacto">Contacto</Nav.Link> 
+          </Nav>
+        </NavbarCollapse>
+
+      </Navbar>
+
+      <div className="content">
+        Cuerpo del sitio.
+      </div>
     </div>
   );
 }
